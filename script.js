17adf4a97cfdad34a444b434193bbc290b910327
GregLed <grzegorz.ledwon@yahoo.com>
2022-01-02 19:56:10 +0100

    added functionality for selecting rating

--- script.js
+++ script.js
@@ -4,10 +4,55 @@ const addReviewBtn = document.getElementById("add-btn");
 const submitReviewBtn = document.getElementById("submit-btn");
 const starPickers = document.querySelectorAll(".picker");
 
+const lightGreyColor = "#bebebe";
+const yellowColor = "#f8e825";
+
+let halfStar;
+let halfStarCached;
+let rating;
+let ratingCached;
+
 starPickers.forEach((star) => {
-  star.addEventListener("mouseenter", (e) => {
-    removeColorPreceedingStars();
-    colorPreceedingStars(+star.id);
+  star.addEventListener("mousemove", (e) => {
+    // Get position within start element
+    const rect = e.target.getBoundingClientRect();
+    const x = e.clientX - rect.left; //x position within the element.
+
+    // If x position less than 10px, user selected half star rating
+    if (x < 10) {
+      halfStar = true;
+    } else {
+      halfStar = false;
+    }
+
+    // Covert to int
+    rating = +star.id;
+
+    removeColorStars();
+    colorPreceedingStars(rating, halfStar);
+  });
+
+  // Remove stars coloring when leaving element
+  star.addEventListener("mouseleave", () => {
+    removeColorStars();
+
+    if (ratingCached) {
+      colorPreceedingStars(ratingCached, halfStarCached);
+    }
+  });
+
+  // Cache rating by user
+  star.addEventListener("click", () => {
+    // If user clicked cached rating, we reset it
+    if (halfStarCached === halfStar && ratingCached === rating) {
+      halfStarCached = null;
+      ratingCached = null;
+      submitReviewBtn.disabled = true;
+    } else {
+      halfStarCached = halfStar;
+      ratingCached = rating;
+      submitReviewBtn.disabled = false;
+    }
   });
 });
 
@@ -21,16 +66,22 @@ submitReviewBtn.addEventListener("click", () => {
   card.classList.add("active");
 });
 
-function removeColorPreceedingStars() {
+function removeColorStars() {
   Array.from(starPickers).forEach((star) => {
-    star.children[0].style.color = "#bebebe";
+    star.children[0].classList = "fas fa-star";
+    star.children[0].style.color = lightGreyColor;
   });
 }
 
-function colorPreceedingStars(rate) {
+function colorPreceedingStars(rate, halfStar) {
   Array.from(starPickers)
     .slice(0, rate)
-    .forEach((star) => {
-      star.children[0].style.color = "#f8e825";
+    .forEach((star, idx) => {
+      if (idx === rate - 1 && halfStar) {
+        star.children[0].classList = "fas fa-star-half-alt";
+        star.children[0].style.color = yellowColor;
+      } else {
+        star.children[0].style.color = yellowColor;
+      }
     });
 }
