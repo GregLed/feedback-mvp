06ff62ae4ab3998cb04c65efe5e47b78ccc134e4
GregLed <grzegorz.ledwon@yahoo.com>
2022-01-02 17:00:33 +0100

    added functionality to add ratings by user

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 const card = document.querySelector(".card");
-const addReviewBtn = document.querySelector(".btn");
+const newReview = document.querySelector(".new-review");
+const addReviewBtn = document.getElementById("add-btn");
+const submitReviewBtn = document.getElementById("submit-btn");
+const starPickers = document.querySelectorAll(".picker");
+
+starPickers.forEach((star) => {
+  star.addEventListener("mouseenter", (e) => {
+    removeColorPreceedingStars();
+    colorPreceedingStars(+star.id);
+  });
+});
 
 addReviewBtn.addEventListener("click", () => {
-  console.log("Button was clicked");
+  card.classList.remove("active");
+  newReview.classList.add("active");
 });
+
+submitReviewBtn.addEventListener("click", () => {
+  newReview.classList.remove("active");
+  card.classList.add("active");
+});
+
+function removeColorPreceedingStars() {
+  Array.from(starPickers).forEach((star) => {
+    star.children[0].style.color = "#bebebe";
+  });
+}
+
+function colorPreceedingStars(rate) {
+  Array.from(starPickers)
+    .slice(0, rate)
+    .forEach((star) => {
+      star.children[0].style.color = "#f8e825";
+    });
+}
