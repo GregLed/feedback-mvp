92a38a881a4a9a834f961a453f8a03b558096704
GregLed <grzegorz.ledwon@yahoo.com>
2022-01-02 16:32:27 +0100

    added a card for new review

---
+++ script.js
@@ -0,0 +1,6 @@
+const card = document.querySelector(".card");
+const addReviewBtn = document.querySelector(".btn");
+
+addReviewBtn.addEventListener("click", () => {
+  console.log("Button was clicked");
+});
